7fc02c912685e24145ff1aadbaf57e40ef11ec01
Tim Kitchens <tkitchens@tkitchens-mac.local>
2020-03-25 17:48:26 -0400

    Updates

---
+++ precache-manifest.918186ac4a7e07c43f22bc8dcb0b4350.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae30dbe07e49b89ea5a46be6a3a5d85d",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "5a3a23fd831b5494d5b0",
+    "url": "/contactmanager/static/css/2.aee84c9b.chunk.css"
+  },
+  {
+    "revision": "2db3fbe52b6df3e2d034",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5a3a23fd831b5494d5b0",
+    "url": "/contactmanager/static/js/2.25df7bc3.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/contactmanager/static/js/2.25df7bc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2db3fbe52b6df3e2d034",
+    "url": "/contactmanager/static/js/main.35386984.chunk.js"
+  },
+  {
+    "revision": "d86057960233a7428d99",
+    "url": "/contactmanager/static/js/runtime-main.2ce66dc9.js"
+  }
+]);
